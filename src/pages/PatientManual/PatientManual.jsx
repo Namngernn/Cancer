3d56c6f330a942d294fb2de979944ad8178302bb
Aphisara Sonthi <nam.apsr.js@gmail.com>
2024-06-23 00:32:46 +0700

    login,register,contacts,patient Done

--- src/pages/PatientManual/PatientManual.jsx
+++ src/pages/PatientManual/PatientManual.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
+import { Button, Typography} from 'antd';
+import { Link } from 'react-router-dom';
 
+const { Text} = Typography;
 const PatientManual = () => {
   return (
-    <div>PatientManual</div>
+    <div>
+      <div className='p-10 text-center sarabun-extralight '>
+          <h1 >Cis CCRT Cervix</h1>
+      </div>
+
+      {/* รูปคิวอาร์ */}
+      <div className="p-20 pt-1">
+        <img src="ing/qr.png" alt=""/>
+      </div>
+
+      <div className='p-20 pt-1 text-center flex flex-col space-y-10'>
+        <Button className='bt-blue '>ดาวน์โหลดข้อมูล</Button>
+        <Link to={'/Contacts'}><Text>ติดต่อโรงพยาบาล</Text></Link>
+      </div>
+
+    </div>
   )
 }
 
